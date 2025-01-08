b55bdd5fb0e317241456a698936ee2bc0feef263
SelmanBilgen <sbilgen40@gmail.com>
2025-01-07 19:02:01 -0500

    Latest

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Pixel Perfect Pizzas")
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Mozzarella cheese, pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Spinach Pizza",
+      description: "Mozzarella cheese, spinach",
+    }),
+    React.createElement(Pizza, {
+      name: "The Mushroom Pizza",
+      description: "Mozzarella cheese, mushrooms",
+    }),
+    React.createElement(Pizza, {
+      name: "The Margarita Pizza",
+      description: "Mozzarella cheese, tomatoes",
+    }),
+  ]);
 };
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
